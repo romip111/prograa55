10008c8458e8d51c83262647a15f2223b4e5984c
romip111 <ro.carrascor@duocuc.cl>
2025-06-26 10:27:43 -0400

    version funcion restar2.0

--- betagithub.py
+++ betagithub.py
@@ -1,6 +1,10 @@
 def sumar (a,b):
     return a+b
+def restar(a,b):
+    return(a - b)
 
 n1 = int(input("Ingresar numero: "))
 n2 = int(input("Ingresar numero: "))
-print(sumar(n1, n2))
+print("La suma es: ", sumar(n1, n2))
+
+print("La resta es: ", restar(n1, n2))
